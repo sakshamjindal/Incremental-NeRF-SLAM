945aa7f395d7ff4798c00fdf4c51d73131a87489
saksham.jindal@outlook.com <saksham.jindal@outlook.com>
2021-08-10 01:50:41 +0530

    point cloud visualisation code added

--- gs2.py
+++ gs2.py
@@ -17,15 +17,22 @@ from gradslam.slam import PointFusion
 from torch.utils.data import DataLoader
 
 path = "/scratch/saksham/datasets/tum"
+poses_path = "inc_nerf_dumps/Nerf_0_phase1/saved_19"
 sequences = "sequences.txt"
 
 # load dataset
-dataset = TUM(path, sequences = sequences , seqlen = 4, height = 60, width = 80)
-loader = DataLoader(dataset=dataset, batch_size=32)
+dataset = TUM(path, sequences = sequences , seqlen = 20, height = 480, width = 640, start = 56)
+loader = DataLoader(dataset=dataset, batch_size=1)
 colors, depths, intrinsics, poses, *_ = next(iter(loader))
 
+# load other dataset
+nerf_outputs = torch.load("renders/Nerf_0")
+colors2 = nerf_outputs["rgb_pred"].unsqueeze(0)
+depths2 = nerf_outputs["depth_pred"].unsqueeze(0).unsqueeze(-1)
+poses2 = torch.load(poses_path)["poses"].unsqueeze(0)
+
 # create rgbdimages object
-rgbdimages = RGBDImages(colors, depths, intrinsics, poses)
+rgbdimages = RGBDImages(colors2, depths2, intrinsics, poses2)
 
 # step by step SLAM
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -33,7 +40,7 @@ slam = PointFusion(device=device)
 
 pointclouds = Pointclouds(device=device)
 pointclouds, recovered_poses = slam(rgbdimages)
-fig = pointclouds.plotly(0, max_num_points=15000).update_layout(autosize=False, width=600)
+fig = pointclouds.plotly(0, max_num_points=15000).update_layout(autosize=False, height = 1200, width=1200)
 
 # Dash
 app = dash.Dash(__name__)
